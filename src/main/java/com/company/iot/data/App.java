90a0c5d35a3a896badf70ada23dace582b49887a
Onur Tokat <onurtkt@gmail.com>
2020-04-05 01:22:53 +0300

    comment check

--- src/main/java/com/company/iot/data/App.java
+++ src/main/java/com/company/iot/data/App.java
@@ -3,7 +3,7 @@ package com.company.iot.data;
 import com.company.iot.data.producer.DataGenerator;
 
 /**
- * App class is main class of Data generator application
+ * App class is main class of Data generator application.
  * @author Onur Tokat
  */
 public class App {
